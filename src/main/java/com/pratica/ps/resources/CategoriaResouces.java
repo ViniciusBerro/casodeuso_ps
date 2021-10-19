4145debd19b8d3b9a43cdc545ae507c268dd9024
ViniciusBerro <vini.berro@gmail.com>
2021-10-18 21:50:13 -0300

    REST testado

--- src/main/java/com/pratica/ps/resources/CategoriaResouces.java
+++ src/main/java/com/pratica/ps/resources/CategoriaResouces.java
@@ -1,13 +1,26 @@
 package com.pratica.ps.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.pratica.ps.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResouces {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
